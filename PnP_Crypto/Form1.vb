a423808d9fe7d8c1f5f5bf3b385dff9a6c12f2d9
DerEineVonDerTanke <clemens.waack@gmail.com>
2018-09-29 17:23:18 +0200

    kp

--- PnP_Crypto/Form1.vb
+++ PnP_Crypto/Form1.vb
@@ -1,6 +1,11 @@
 ﻿Imports Newtonsoft.Json
 Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.publickey = Nothing Then
+            Funktion.Create_adress()
+            My.Settings.Save()
+        End If
 
+        Process.Start("https://www.blockchain.com/de/btc/address/" + My.Settings.publickey)
     End Sub
 End Class
--- PnP_Crypto/funktion.vb
+++ PnP_Crypto/funktion.vb
@@ -1,5 +1,29 @@
-﻿Public Class Funktion
-    Public Sub Create_adress()
+﻿Imports System.Net
+Imports System.Security.Cryptography
+Imports System.Text
+Imports Bitcoin
+Imports Bitcoin.BitcoinUtilities
+Imports Bitcoin.KeyCore
+Imports System.Net.WebClient
+Imports Newtonsoft.Json
+
+Public Class Funktion
+    Shared Sub Create_adress()
+        Dim randomtext = Convert.ToBase64String(Utilities.GetRandomBytes(39)).Replace(vbCr, "r"c).Replace(vbLf, "n"c)
+        Dim keyBytes As Byte() = New SHA256Managed().ComputeHash(UTF8Encoding.UTF8.GetBytes(randomtext), 0, UTF8Encoding.UTF8.GetBytes(randomtext).Length)
+        Dim pkInitialWif As PrivateKey = New PrivateKey(Globals.ProdDumpKeyVersion, keyBytes)
+        Dim newprivatekey = pkInitialWif.WIFEncodedPrivateKeyString
+
+        Dim newpublickey As New PublicKey(pkInitialWif)
+        My.Settings.privatekey = newprivatekey
+
+        My.Settings.publickey = KeyCore.BitcoinAddress.GetBitcoinAdressEncodedStringFromPublicKey(newpublickey)
 
     End Sub
+
+    Shared Function get_kontostand(pubkey As String) As String
+        Dim kontostand As New WebClient
+        Dim ergebnis = JsonConvert.SerializeObject(kontostand.OpenRead("https://chain.so/api/v2/get_address_balance/BTC/" + pubkey))
+
+    End Function
 End Class
